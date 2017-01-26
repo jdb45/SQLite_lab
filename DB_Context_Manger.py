e1460edf3e60d5c2873ef32d57b0f44398852ec1
Jeremy <acnajcadcac.yahoo.com>
2017-01-26 16:36:34 -0600

    added comments

--- DB_Context_Manger.py
+++ DB_Context_Manger.py
@@ -5,24 +5,26 @@ import User_Interface
 # Create a table
 def setup():
     try:
-        db = sqlite3.connect('record_holder_db.db')  # Creates or opens database file
-        cur = db.cursor()  # Need a cursor object to perform operations
+        # Creates or opens the database file
+        db = sqlite3.connect('record_holder_db.db')
+        cur = db.cursor()
+        # creating the table
         cur.execute('create table record_holder (Chainsaw_Juggling_Record_Holder text, Country text, Number_of_catches int)')
 
         try:
-            db = sqlite3.connect('record_holder_db.db')  # Creates or opens database file
-            cur = db.cursor()  # Need a cursor object to perform operations
+            # I was not sure if I needed this. I will test when I have more time
+            db = sqlite3.connect('record_holder_db.db')
+            cur = db.cursor()
 
             with db:
+                #adding records to the table
                 cur.execute('insert into record_holder values ("Ian Stewart", "Canada", 94)')
                 cur.execute('insert into record_holder values ("Aaron Gregg", "Canada", 88)')
                 cur.execute('insert into record_holder values ("Chad Taylor", "USA", 78)')
 
-                # db.commit()  # Don't need - will be called automatically by the context manager if there's no error.
         except sqlite3.Error as e:
             print('Error adding rows')
             print(e)
-            # In the event of an error, transactions will be automatically rolled back.
 
     except sqlite3.Error:
         print()
@@ -31,7 +33,7 @@ def setup():
         db.close()
 
 def handle_choice(choice):
-
+    #handler menu
     if choice == '1':
         insert_row()
 
@@ -55,10 +57,11 @@ def handle_choice(choice):
 
 def delete_row():
     try:
-        db = sqlite3.connect('record_holder_db.db')  # Creates or opens database file
-        cur = db.cursor()  # Need a cursor object to perform operations
+        db = sqlite3.connect('record_holder_db.db')
+        cur = db.cursor()
+        #getting the input from the user
         record_delete = input('Please enter the name of the record holder to delete:')
-
+        # deleting a record
         cur.execute('DELETE FROM record_holder WHERE Chainsaw_Juggling_Record_Holder = (?)', (record_delete,))
         db.commit()
     except sqlite3.Error as e:
@@ -70,10 +73,12 @@ def delete_row():
 
 def update_catches():
     try:
-        db = sqlite3.connect('record_holder_db.db')  # Creates or opens database file
-        cur = db.cursor()  # Need a cursor object to perform operations
+        db = sqlite3.connect('record_holder_db.db')
+        cur = db.cursor()
+        # getting the information to update the record
         record_name = input('Please enter the name of the record holder to update:')
         record_catches = input('Please enter the amount of catches you want to update:')
+        #updating the catches for the record holder
         cur.execute('UPDATE record_holder SET Number_of_catches = (?) WHERE Chainsaw_Juggling_Record_Holder = (?)', (record_catches, record_name,))
         db.commit()
     except sqlite3.Error as e:
@@ -84,10 +89,10 @@ def update_catches():
         db.close()
 
 def view_records():
-    # Execute a query. Do not need a context manager, as no changes are being made to the DB
+    #view all recoreds in the DB
     try:
-        db = sqlite3.connect('record_holder_db.db')  # Creates or opens database file
-        cur = db.cursor()  # Need a cursor object to perform operations
+        db = sqlite3.connect('record_holder_db.db')
+        cur = db.cursor()
 
         for row in cur.execute('select * from record_holder'):
             print(row)
@@ -100,11 +105,11 @@ def view_records():
         db.close()
 
 def search_records():
-    # Execute a query. Do not need a context manager, as no changes are being made to the DB
-    try:
-        db = sqlite3.connect('record_holder_db.db')  # Creates or opens database file
-        cur = db.cursor()  # Need a cursor object to perform operations
 
+    try:
+        db = sqlite3.connect('record_holder_db.db')
+        cur = db.cursor()
+        #getting input to search for a record
         record_search = input('Please enter the name of the record holder you want to search for:')
 
         for row in cur.execute('select * from record_holder WHERE Chainsaw_Juggling_Record_Holder = (?)', (record_search,)):
@@ -118,29 +123,26 @@ def search_records():
         db.close()
 
 def insert_row():
-    db = sqlite3.connect('record_holder_db.db')  # Creates or opens database file
+    db = sqlite3.connect('record_holder_db.db')
 
-    cur = db.cursor()  # Need a cursor object to perform operations
+    cur = db.cursor()
 
-    # Create a table if not exists...
     cur.execute('create table if not exists record_holder (Chainsaw_Juggling_Record_Holder text, Country text, Number_of_catches int)')
 
-    # Ask user for information for a new phone
+    # Ask user for information for the record holder
     name = input('Enter name of record holder: ')
     country = input('Enter the Country of the record holder: ')
     number_catches = int(input('Enter the number of catches (as an integer): '))
 
-    # Parameters. Use a ? as a placeholder for data that will be filled in
-    # Provide data as a second argument to .execute, as a tuple of values
+    # inserting the new data into the table
     cur.execute('insert into record_holder values (?, ?, ?)', (name, country, number_catches))
 
-    # Fetch and display all data. Results stored in the cursor object
     cur.execute('select * from record_holder')
 
     for row in cur:
         print(row)
 
-    db.commit()  # Ask the database to save changes!
+    db.commit()  
 
 
 def main():
